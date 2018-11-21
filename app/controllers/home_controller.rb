0e652b856dac57aed1a7ab5f4d87fe2153eac0a5
Rob Brown <brown4homes@yahoo.com>
2018-11-21 14:54:04 -0800

    Added functionality to form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-    
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter a symbol"
+    elsif
+      if params[:id]
+         @stock = StockQuote::Stock.quote(params[:id]) 
+       end
+     end    
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
+  get 'home/index'
+
   get 'home/about'
+
+  post "/" => 'home#index'
   
 end

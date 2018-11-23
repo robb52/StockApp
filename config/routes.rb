bdd28375d1bf7fa3ca4deb33edd2cea2869bd387
Rob Brown <brown4homes@yahoo.com>
2018-11-23 15:43:20 -0800

    Stock scaffold and stlyed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :stocks
+
   devise_for :users
   
   root 'home#index'
-
-  get 'home/index'
 
   get 'home/about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_192207) do
+ActiveRecord::Schema.define(version: 2018_11_23_233616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
